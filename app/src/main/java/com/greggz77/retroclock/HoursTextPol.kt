00fdc26ee45782230a693ac1b65ce66d20afe057
gregz <gregor@zafran.si>
2019-11-25 17:22:38 +0100

    displays time with text

--- app/src/main/java/com/greggz77/retroclock/HoursTextPol.kt
+++
@@ -1,6 +0,0 @@
-package com.greggz77.retroclock
-
-class HoursTextPol {
-    val hoursTextMapPol
-            = mapOf<String, Int>("Enih" to 1, "Dveh" to 2, "Treh" to 3, "Štirih" to 4, "Petih" to 5, "Šestih" to 6, "Sedmih" to 7, "Osmih" to 8, "Devetih" to 9, "Desetih" to 10, "Enajstih" to 11, "Dvanajstih" to 12)
-}
--- app/src/main/java/com/greggz77/retroclock/HoursTextTri.kt
+++
@@ -1,6 +0,0 @@
-package com.greggz77.retroclock
-
-class HoursTextTri {
-    val hoursTextMapTricetrt
-            = mapOf<String, Int>("Ena" to 1, "Dve" to 2, "Tri" to 3, "Štiri" to 4, "Pet" to 5, "Šest" to 6, "Sedem" to 7, "Osem" to 8, "Devet" to 9, "Deset" to 10, "Enajst" to 11, "Dvanajst" to 12)
-}
